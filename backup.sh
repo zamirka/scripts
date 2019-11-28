3b07567aee8d78fc6eaba7d24235c7b670fb813e
Zamir Musaev <zamir.ru@gmail.com>
2019-11-28 15:25:10 +0300

    Modify to load to cloud by webdav to save local space

--- backup.sh
+++ backup.sh
@@ -3,31 +3,47 @@
 # and copy to cloud
 # split archive into pieces in order to fit cloud limit
 
-# archive file name 
-filename=$(date '+%d-%m-%Y')
+# Memorize current directory to access credentials file
+cur_dir="$(dirname "$0")"
+# credentials to access Cloud
+. "$cur_dir/credentials.config"
 
-# Delete archive volumes older than 1 minute
-find /Users/zamir/cloud.mail/backup/* -type d -ctime +13d -exec rm -rf {} \;
+# archive file name 
+filename=$(date '+%d-%m-%Y-%H-%M-%S')
+
+# Delete archive volumes from Cloud older than 11 days
+find /Volumes/Backup/backup/** -type d -ctime +2s -exec sh -c \
+'f="{}";duck -q -y -u '$login' -p '$password' \
+-D https://webdav.cloud.mail.ru/backup/"${f##*/}"' \;
+
+# Delete archive volumes from USB drive older than 11 days
+find /Volumes/Backup/backup/** -type d -ctime +13d -exec rm -rf {} \;
 
 # Create archive volume folder
-mkdir /Users/zamir/cloud.mail/backup/$filename
+mkdir /Volumes/Backup/backup/$filename
+
 
 # Archive target folders
 tar -zcvf "/Users/zamir/temp/$filename.tar" \
 /Users/zamir/Documents/ \
-/Users/zamir/cloud.mail/KeePass/ \
 /Users/zamir/Movies/video_archive/ \
 /Users/zamir/Pictures/photo_archive/;
 
-# Split archive into chunks to fit cloud
+# Split archive into chunks to file system
 split -b 1900m /Users/zamir/temp/$filename.tar \
-/Users/zamir/cloud.mail/backup/$filename/$filename;
+/Volumes/Backup/backup/$filename/$filename;
 
 # Remove original archive file
 rm -rf /Users/zamir/temp/$filename.tar
 
-# Delete archive volumes from USB drive older than 11 days minute
-find /Volumes/Backup/backup/** -type d -ctime +13d -exec rm -rf {} \;
 
-#Copy archive volume to USB drive
-cp -r /Users/zamir/cloud.mail/backup/$filename /Volumes/Backup/backup
+# Upload files to rhe cloud using WebDav access
+duck -q -y -u $login -p $password --nokeychain \
+--synchronize https://webdav.cloud.mail.ru/  /Volumes/Backup/backup/ \
+--parallel 4 --existing mirror;
+
+# Clear environment variables
+unset login
+unset password
+unset filename
+
